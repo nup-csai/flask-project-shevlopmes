c041f6115c2b6815f712e1fe743733a2a91b3d07
shevlopmes <makeng2006@gmail.com>
2024-11-12 13:26:55 +0200

    object Player and allinfo

--- server.py
+++ server.py
@@ -1,10 +1,26 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+class Player:
+    def __init__ (self, name, status):
+        self.name = name
+        self.status = status
+        self.wins= 0
+        self.losses =0
+    def change_name (self,name):
+        self.name = name
+    def change_status(self,status):
+        self.status = status
+    def add_win(self, add):
+        self.wins += add
+    def add_loss(self,add):
+        self.losses+=add
+p = [Player("$i","active",0,0) for i in range(5)]
 @app.route('/')
 def home():
     return 'Hello World!'
-
+@app.route('/getall/<int=id>')
+def allinfo(id):
+    return '$p[id].name \n$p[id].status \n$p[id].wins \n$p[id].losses'
 if (__name__ == "__main__"):
     home()
