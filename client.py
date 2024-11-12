fe0cecd5b9930d99cdfebfbc8f6d34d5642cee05
shevlopmes <makeng2006@gmail.com>
2024-11-12 14:47:18 +0200

    client added

---
+++ client.py
@@ -0,0 +1,17 @@
+import requests
+import sys
+def main():
+    response  = requests.get("http://localhost:8080/")
+    if response.status_code == 200:
+        print("Start page OK")
+    else:
+        print("Start page failed")
+        sys.exit(1)
+    response = requests.get("http://localhost:8080/get/0/name")
+    if response.status_code == 200:
+        print("Get first name OK")
+    else:
+        print("Get first name failed")
+        sys.exit(1)
+if(__name__ == "__main__"):
+    main()
