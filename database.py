b859e373a0abc5f805757d0428b1b9118b5e5c41
shevlopmes <makeng2006@gmail.com>
2024-12-23 16:45:03 +0200

    some htmls added

--- database.py
+++ database.py
@@ -1,1 +1,98 @@
-a = []
+import sqlite3
+from threading import Lock
+
+
+import flask_sqlalchemy
+class Database:
+    _instance = None
+    _lock = Lock()
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            with cls._lock:
+                if not cls._instance:
+                    cls._instance = super(Database,cls).__new__(cls)
+                    cls._instance.__init__(*args,**kwargs)
+        return cls._instance
+
+    def __init__(self,db_path=":memory:"):
+        self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor()
+        self.game_count = 0
+        self.player_count = 0
+        self._create_test()
+
+    def _create_test(self):
+        self.cursor.execute('create table if not exists players (id int primary key, name text not null, status text not null)')
+        self.cursor.execute('create table if not exists games (id int primary key, score_1 int, score_2 int, pl_1_id int, pl_2_id int, foreign key (pl_1_id) references players(id), foreign key (pl_2_id) references players(id))')
+        self.conn.commit()
+
+    def new_game(self, first_pl, second_pl):
+        self.cursor.execute('select a.id from players a where a.name = ?', first_pl)
+        id1 = self.cursor.fetchall()
+        self.cursor.execute('select a.id from players a where a.name = ?', second_pl)
+        id2 = self.cursor.fetchall()
+        self.cursor.execute('insert into games (id, score_1, score_2, pl_1_id, pl_2_id) values (?,0,0,?,?)',(self.game_count,id1,id2))
+        try:
+            self.conn.commit()
+            self.game_count += 1
+            return self.game_count
+        except Exception:
+            return -1
+
+    def new_player(self,name):
+        self.cursor.execute('insert into players (id, name, status) values (?,?,"live")',(self.player_count,name))
+        self.player_count+=1
+        self.conn.commit()
+
+    def one_game(self, game_id):
+        if game_id < 0 or game_id >= self.game_count:
+            return None
+        a = list()
+        self.cursor.execute('select * from games a where a.id = ?',game_id)
+        self.conn.commit()
+        tmp = self.cursor.fetchall()
+        a['first_sc'] = tmp['score_1']
+        a['second_sc'] = tmp['score_2']
+        a['live'] = 0
+        id1 = tmp['pl_1_id']
+        id2 = tmp['pl_2_id']
+        self.cursor.execute('select a.name from players a where a.id = ?',id1)
+        self.conn.commit()
+        a['first_pl'] = self.cursor.fetchall()
+        self.cursor.execute('select a.name from players a where a.id = ?', id2)
+        self.conn.commit()
+        a['second_pl'] = self.cursor.fetchall()
+        return a
+    def change_game (self, game_id, live_status, sc_1, sc_2):
+        self.cursor.execute('update games set score_1 = ? score_2 = sc_2 where id = ?',(sc_1,sc_2,game_id))
+        self.conn.commit()
+    def all_games(self):
+        output = []
+        for i in range(self.game_count):
+            output.append(self.one_game(self,i))
+        return output
+
+
+def get_all_games(contest_id):
+    d = Database('bbba.db')
+    return d.all_games()
+def get_one_game(contest_id, game_id):
+    d = Database('bbba.db')
+    return d.one_game(game_id)
+
+def change_game(contest_id,game_id,live_status,sc_1,sc_2):
+    d = Database('bbba.db')
+    return d.change_game(game_id,live_status,sc_1, sc_2)
+def add_one_game(contest_id,p1,p2):
+    d = Database('bbba.db')
+    return d.new_game(p1,p2)
+
+def add_one_player(contest_id,p):
+    d = Database('bbba.db')
+    print(d.player_count)
+    return d.new_player(p)
+
+
+
+
+
--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from database import get_all_games
 from database import get_one_game
 from database import change_game
-from database import get_scoreboard
-from database import create_contest
-from database import add
-from database import remove
+from database import add_one_game
+from database import add_one_player
+
+#from database import get_scoreboard
+#from database import create_contest
+#from database import add
+#from database import remove
 app = Flask(__name__)
 
 
@@ -14,74 +17,102 @@ def home():
     return render_template('base_page.html')
 
 
-@app.route('/contests/<int: contest_id>/games', methods=['GET'])
+@app.route('/contests/<int:contest_id>/games', methods=['GET'])
 def all_games(contest_id):
     output = get_all_games(contest_id)
+    print(output)
     if output is None:
         return render_template('incorrect_page.html')
     return render_template('games.html', games=output)
 
 
-@app.route('/contests/<int: contest_id>/games/<int: game_id>', methods = ['GET'])
+@app.route('/contests/<int:contest_id>/games/<int:game_id>', methods = ['GET'])
 def one_game(contest_id, game_id):
     output = get_one_game(contest_id, game_id)
     if output is None:
         return render_template('incorrect_page.html')
-    return render_template('one_game.html', game=output)
+    return render_template('one_game.html', game=output, contest_id=contest_id)
 
-@app.route('/contests/<int: contest_id>/games/<int: game_id>', methods = ['POST'])
+@app.route('/contests/<int:contest_id>/games/<int:game_id>', methods = ['POST'])
 def change(contest_id, game_id):
     live_status = request.form.get('live')
-    p1 = request.form.get('p1')
-    p2 = request.form.get('p2')
+    p1 = request.form.get('first_sc')
+    p2 = request.form.get('second_sc')
     change_game(contest_id,game_id,live_status,p1,p2)
     output = get_one_game(contest_id, game_id)
     if output is None:
         return render_template('incorrect_page.html')
-    return render_template('one_game.html',game=output)
+    return render_template('one_game.html',game=output,contest_id=contest_id)
 
-@app.route('/contests/<int: contest_id>/scoreboard', methods = ['GET'])
-def scoreboard(contest_id):
-    participants_sorted = get_scoreboard(contest_id,0)
-    if participants_sorted is None:
+@app.route('/contests/<int:contest_id>/games/add', methods = ['GET'])
+def show_add_game(contest_id):
+    return render_template('add_game.html', contest_id = contest_id)
+
+@app.route('/contests/<int:contest_id>/games/add', methods = ['POST'])
+def add_game(contest_id):
+    p1 = request.form.get('first_sc')
+    p2 = request.form.get('second_sc')
+    id = add_one_game(contest_id,p1,p2)
+    output =  get_one_game(contest_id,id)
+    if output is None:
         return render_template('incorrect_page.html')
-    return render_template('scoreboard.html', scoreboard = participants_sorted)
+    return render_template('one_game.html',game=output,contest_id=contest_id)
 
-@app.route('/contests/<int: contest_id>/scoreboard/live', methods = ['GET'])
-def scoreboard(contest_id):
-    participants_sorted = get_scoreboard(contest_id,1)
-    if participants_sorted is None:
-        return render_template('incorrect_page.html')
-    return render_template('scoreboard.html', scoreboard = participants_sorted)
+@app.route('/contests/<int:contest_id>/add', methods = ['GET'])
+def show_add_player(contest_id):
+    return render_template('add_player.html', contest_id=contest_id)
 
-@app.route('/new_contest', methods=['GET'])
-def new_contest():
-    new_id = create_contest()
-    return render_template('new_contest.html', id=new_id)
-
-@app.route('/<int: contest_id>/add_player/result', methods= ['POST'])
+@app.route('/contests/<int:contest_id>/add', methods = ['POST'])
 def add_player(contest_id):
-    player_name = request.form.get('name')
-    if(add(contest_id, player_name) == -1):
-        return render_template('incorrect_page.html')
-    else:
-        return render_template('added.html',id=contest_id, name=player_name)
+    n= request.form.get('first_sc')
+    add_one_player(contest_id,n)
+    return render_template('added.html')
 
-@app.route('/<int: contest_id>/remove_player', methods = ['GET'])
-def show_form(contest_id):
-    return render_template('form_for_removing.html', id=contest_id)
 
-@app.route('/<int: contest_id>/remove_player/result', methods= ['POST'])
-def add_player(contest_id):
-    player_name = request.form.get('name')
-    if(add(contest_id, player_name) == -1):
-        return render_template('incorrect_page.html')
-    else:
-        return render_template('removed.html',id=contest_id, name=player_name)
 
-@app.route('/<int: contest_id>/remove_player', methods = ['GET'])
-def show_form(contest_id):
-    return render_template('form_for_removing.html', id=contest_id)
+
+# @app.route('/contests/<int: contest_id>/scoreboard', methods = ['GET'])
+# def scoreboard(contest_id):
+#     participants_sorted = get_scoreboard(contest_id,0)
+#     if participants_sorted is None:
+#         return render_template('incorrect_page.html')
+#     return render_template('scoreboard.html', scoreboard = participants_sorted)
+#
+# @app.route('/contests/<int: contest_id>/scoreboard/live', methods = ['GET'])
+# def scoreboard(contest_id):
+#     participants_sorted = get_scoreboard(contest_id,1)
+#     if participants_sorted is None:
+#         return render_template('incorrect_page.html')
+#     return render_template('scoreboard.html', scoreboard = participants_sorted)
+#
+# @app.route('/new_contest', methods=['GET'])
+# def new_contest():
+#     new_id = create_contest()
+#     return render_template('new_contest.html', id=new_id)
+#
+# @app.route('/<int: contest_id>/add_player/result', methods= ['POST'])
+# def add_player(contest_id):
+#     player_name = request.form.get('name')
+#     if(add(contest_id, player_name) == -1):
+#         return render_template('incorrect_page.html')
+#     else:
+#         return render_template('added.html',id=contest_id, name=player_name)
+#
+# @app.route('/<int: contest_id>/remove_player', methods = ['GET'])
+# def show_form(contest_id):
+#     return render_template('form_for_removing.html', id=contest_id)
+#
+# @app.route('/<int: contest_id>/remove_player/result', methods= ['POST'])
+# def add_player(contest_id):
+#     player_name = request.form.get('name')
+#     if(remove(contest_id, player_name) == -1):
+#         return render_template('incorrect_page.html')
+#     else:
+#         return render_template('removed.html',id=contest_id, name=player_name)
+#
+# @app.route('/<int: contest_id>/remove_player', methods = ['GET'])
+# def show_form(contest_id):
+#     return render_template('form_for_removing.html', id=contest_id)
 
 
 if __name__ == '__main__':
